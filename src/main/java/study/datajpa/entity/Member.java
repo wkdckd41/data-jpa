98fca5e3dc961afa35f27a6f8c78960ac1fba7e3
wkdckd41 <wkdckd41@gmail.com>
2024-03-18 09:52:55 +0900

    2. 예제 도메인 모델 - 예제 도메인 모델과 동작확인

--- src/main/java/study/datajpa/entity/Member.java
+++ src/main/java/study/datajpa/entity/Member.java
@@ -1,23 +1,38 @@
 package study.datajpa.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "username", "age"})
 public class Member {
 
     @Id @GeneratedValue
+    @Column(name = "member_id")
     private Long id;
     private String username;
+    private int age;
 
-    protected Member() {
-    }
+    @ManyToOne(fetch = FetchType.LAZY) // 지연로딩
+    @JoinColumn(name = "team_id") // 외래키 매핑
+    private Team team;
 
     public Member(String username) {
         this.username = username;
+    }
+
+    public Member(String member1, int i, Team team) {
+        this.username = member1;
+        this.age = i;
+        if (team != null) {
+            changeTeam(team);
+        }
+    }
+
+    public void changeTeam(Team team) {
+        this.team = team;
+        team.getMembers().add(this);
     }
 }
