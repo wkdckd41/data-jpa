65f7cc15469e4b118a1e04e41cb1745822b14ffb
wkdckd41 <wkdckd41@gmail.com>
2024-03-19 18:34:19 +0900

    4. 쿼리 메소드 기능 - 파라미터 바인딩

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -9,6 +9,7 @@ import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 import study.datajpa.entity.Team;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -112,6 +113,19 @@ void basicCRUD() {
         List<MemberDto> memberDtos = memberRepository.fidMemberDto();
         for (MemberDto memberDto : memberDtos) {
             System.out.println("memberDto = " + memberDto);
+        }
+    }
+
+    @Test
+    void findByNames() {
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("BBB", 20);
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        List<Member> result = memberRepository.findByNames(Arrays.asList("AAA", "BBB"));
+        for (Member member : result) {
+            System.out.println("member = " + member);
         }
     }
 }
