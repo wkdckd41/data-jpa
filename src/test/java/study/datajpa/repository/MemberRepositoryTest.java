4dc11fed67e33ced88f53bca6217ab071f6fcf9d
wkdckd41 <wkdckd41@gmail.com>
2024-03-19 16:49:32 +0900

    3. 공통 인터페이스 기능 - 공통 인터페이스 적용

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -26,4 +26,29 @@ class MemberRepositoryTest {
         assertEquals(findMember.getUsername(), member.getUsername());
         assertEquals(findMember, member);
     }
+
+    @Test
+void basicCRUD() {
+        Member member1 = new Member("member1");
+        Member member2 = new Member("member2");
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        // 단건 조회 검증
+        Member findMember1 = memberRepository.findById(member1.getId()).get();
+        Member findMember2 = memberRepository.findById(member2.getId()).get();
+        assertEquals(findMember1, member1);
+        assertEquals(findMember2, member2);
+
+        // 리스트 조회 검증
+        assertEquals(memberRepository.findAll().size(), 2);
+
+        // 카운트 검증
+        assertEquals(memberRepository.count(), 2);
+
+        // 삭제 검증
+        memberRepository.delete(member1);
+        memberRepository.delete(member2);
+        assertEquals(memberRepository.count(), 0);
+    }
 }
