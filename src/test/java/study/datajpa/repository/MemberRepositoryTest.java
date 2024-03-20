cb949d5544edbb895639747df265e6526abd8b73
wkdckd41 <wkdckd41@gmail.com>
2024-03-20 19:14:52 +0900

    5. 확장기능 - 사용자 정의 리포지토리 구현

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -266,4 +266,9 @@ class MemberRepositoryTest {
         // when
         List<Member> result = memberRepository.findLockByUsername("member1");
     }
+
+    @Test
+    void callCustom() {
+        List<Member> result = memberRepository.findMemberCustom();
+    }
 }
